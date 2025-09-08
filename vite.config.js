35e87daa503f4a633061c1b338844de8b4cc8acf
Adrian Hansen <adrian.hansen.flensburg@gmail.com>
2025-09-08 11:23:38 +0200

    add subpages to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import {defineConfig} from "vite";
 
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
     base: "/interpolations-bezier-demo",
+
+    rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'index.html'),
+            hermiteInterpolation: resolve(__dirname, 'hermiteInterpolation.html'),
+            interpolateByPolynomial: resolve(__dirname, 'interpolateByPolynomial.html'),
+            rungeFunction: resolve(__dirname, 'rungeFunction.html')
+        }}
 
 })
